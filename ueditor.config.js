6f7afb9064f7b6bfca2071003682fdbf52643b83
ipbbs <ipbbs@sin.(none)>
2014-10-29 15:40:28 +0800

    config.js

---
+++ ueditor.config.js
@@ -0,0 +1,52 @@
+﻿
+(function () {
+
+    var URLM = window.UEDITOR_HOME_URL || getUEBasePath();
+    var URL = 'http://ueditor.baidu.com/ueditor/';
+    window.UEDITOR_CONFIG = {
+        UEDITOR_HOME_URL: URL
+        , serverUrl: URLM + "php/controller.php"
+        , toolbars: [["fullscreen","simpleupload","insertcode","horizontal","forecolor","fontsize","bold",]]
+    };
+
+    function getUEBasePath(docUrl, confUrl) {
+        return getBasePath(docUrl || self.document.URL || self.location.href, confUrl || getConfigFilePath());
+    }
+    function getConfigFilePath() {
+        var configPath = document.getElementsByTagName('script');
+        return configPath[ configPath.length - 1 ].src;
+    }
+    function getBasePath(docUrl, confUrl) {
+        var basePath = confUrl;
+
+        if (/^(\/|\\\\)/.test(confUrl)) {
+            basePath = /^.+?\w(\/|\\\\)/.exec(docUrl)[0] + confUrl.replace(/^(\/|\\\\)/, '');
+        } else if (!/^[a-z]+:/i.test(confUrl)) {
+            docUrl = docUrl.split("#")[0].split("?")[0].replace(/[^\\\/]+$/, '');
+            basePath = docUrl + "" + confUrl;
+        }
+        return optimizationPath(basePath);
+    }
+
+    function optimizationPath(path) {
+        var protocol = /^[a-z]+:\/\//.exec(path)[ 0 ],
+            tmp = null,
+            res = [];
+
+        path = path.replace(protocol, "").split("?")[0].split("#")[0];
+        path = path.replace(/\\/g, '/').split(/\//);
+        path[ path.length - 1 ] = "";
+        while (path.length) {
+            if (( tmp = path.shift() ) === "..") {
+                res.pop();
+            } else if (tmp !== ".") {
+                res.push(tmp);
+            }
+        }
+        return protocol + res.join("/");
+    }
+    window.UE = {
+        getUEBasePath: getUEBasePath
+    };
+
+})();
